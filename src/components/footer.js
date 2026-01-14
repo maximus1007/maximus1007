d913551a3f28c703f90b07a4f3f0b3272ecb8c7f
maximus1007 <dlint909@gmail.com>
2026-01-14 16:47:03 +0800

    fixed some issue

--- src/components/footer.js
+++ src/components/footer.js
@@ -72,23 +72,6 @@ const Footer = () => {
     stars: null,
     forks: null,
   });
-
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
-
   return (
     <StyledFooter>
       <StyledSocialLinks>
@@ -105,21 +88,8 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="https://github.com/maximus1007/maximus1007">
           <div>Designed &amp; Built by David Liang</div>
-
-          {githubInfo.stars && githubInfo.forks && (
-            <div className="github-stats">
-              <span>
-                <Icon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <Icon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </div>
-          )}
         </a>
       </StyledCredit>
     </StyledFooter>
